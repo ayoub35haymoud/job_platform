53ba1e1f073f2650b0d5546d2d521341a603cd3b
ayoub <haymoudayoub50@gmail.com>
2025-05-31 17:34:02 +0200

    create the hero section

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
-     theme: {
-    extend: {
-      keyframes: {
-        floatY: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-20px)' },
-        },
-      },
-      animation: {
-        floatY: 'floatY 3s ease-in-out infinite',
-      },
-    },
-    },
+     theme: {},
     plugins: [require('@tailwindcss/forms')],
 }
+
+  //  extend: {
+  //       keyframes: {
+  //         floatY: {
+  //           '0%, 100%': { transform: 'translateY(0)' },
+  //           '50%': { transform: 'translateY(-20px)' },
+  //         },
+  //       },
+  //       animation: {
+  //         floatY: 'floatY 3s ease-in-out infinite',
+  //       },
+  //     },
